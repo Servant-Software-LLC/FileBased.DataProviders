732b47b4662c0e51f2b1bb7fc742d641641b76a9
shahidkhan.dev88@gmail.com <shahidkhan.dev88@gmail.com>
2023-02-08 13:26:46 +0500

    renamed test methods names delete unused files

--- tests/Data.Json.Tests/ReaderTests.cs
+++ tests/Data.Json.Tests/ReaderTests.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Data.Json.Tests.FolderAsDb
+namespace Data.Json.Tests
 {
     public class ReaderTests
     {
--- tests/Data.Json.Tests/UpdateTests.cs
+++ tests/Data.Json.Tests/UpdateTests.cs
@@ -1,43 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.JsonClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.JsonClient;
 using Xunit;
 
 namespace Data.Json.Tests
 {
     public class UpdateTests
     {
-        string connectionString = "Data Source=Sources/database.json;";
-
-
+        #region Folder as DataBase
         [Fact]
         public void Update_ShouldUpdateData()
         {
             // Arrange
-            var connection = new JsonConnection(connectionString);
-            var command = new JsonCommand("UPDATE [employees] SET salary=60000 WHERE name='Mike'", connection);
+            JsonConnection connection = new JsonConnection(ConnectionStrings.FolderAsDBConnectionString);
+            JsonCommand command = new JsonCommand();
+            connection.Open();
+            command.Connection = connection;
 
+            // Insert data in both tables
+            command.CommandText = "INSERT INTO employees (name,email,salary,married) values ('John khan','johndoe@email.com',50000,'true')";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO locations (id,city,state,zip) values (440,'New York','NY',10001)";
+            command.ExecuteNonQuery();
 
             // Act
-            connection.Open();
-            var result = command.ExecuteNonQuery();
+            command.CommandText = "UPDATE employees SET salary=60000 where name='John khan'";
+            int employeesUpdated = command.ExecuteNonQuery();
+
+            command.CommandText = "UPDATE locations SET city='Los Angeles' where id=440";
+            int locationsUpdated = command.ExecuteNonQuery();
 
             // Assert
-            Assert.Equal(1, result);
-            connection = new JsonConnection(connectionString);
-            connection.Open();
-            var readCommand = new JsonCommand("SELECT * FROM [employees] WHERE name='Mike'", connection);
-            var reader = readCommand.ExecuteReader();
-            while (reader.Read())
-            {
-                Assert.Equal(60000M, reader["salary"]);
-            }
-            reader.Dispose();
-            connection.Close();
+            Assert.Equal(1, employeesUpdated);
+            Assert.Equal(1, locationsUpdated);
+
+            // Clean up the data from files
+            command.CommandText = "DELETE FROM employees where name='John khan'";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "DELETE FROM locations where id=440";
+            command.ExecuteNonQuery();
         }
 
+        #endregion
+
+        #region File as database
+
+        [Fact]
+        public void Update_ShouldUpdateDataInFile()
+        {
+            // Arrange
+            JsonConnection connection = new JsonConnection(ConnectionStrings.FileAsDBConnectionString);
+            JsonCommand command = new JsonCommand();
+            connection.Open();
+            command.Connection = connection;
+
+            // Insert data in both tables
+            command.CommandText = "INSERT INTO employees (name,email,salary,married) values ('KHAN','johndoe@email.com',50000,'true')";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO locations (id,city,state,zip) values (12310,'New York','NY',10001)";
+            command.ExecuteNonQuery();
+
+            // Act
+            command.CommandText = "UPDATE employees SET salary=60000 where name='KHAN'";
+            int employeesUpdated = command.ExecuteNonQuery();
+
+            command.CommandText = "UPDATE locations SET city='Los Angeles' where id=12310";
+            int locationsUpdated = command.ExecuteNonQuery();
+
+            // Assert
+            Assert.Equal(1, employeesUpdated);
+            Assert.Equal(1, locationsUpdated);
+
+            // Clean up the data from files
+            command.CommandText = "DELETE FROM employees where name='KHAN'";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "DELETE FROM locations where id=12310";
+            command.ExecuteNonQuery();
+        }
+
+        #endregion
     }
 }
