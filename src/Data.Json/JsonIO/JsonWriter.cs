f2312d526a21648474f70bf9512a177a639e265f
David Maltby <david.maltby@hotmail.com>
2023-02-11 20:42:16 -0600

    Remove duplicate code for writing json table in either "Folder as Database" or "File as Database"

--- src/Data.Json/JsonIO/JsonWriter.cs
+++ src/Data.Json/JsonIO/JsonWriter.cs
@@ -39,44 +39,12 @@ public abstract class JsonWriter
             jsonWriter.WriteStartObject();
             foreach (DataTable table in jsonConnection.JsonReader.DataSet!.Tables)
             {
-                jsonWriter.WriteStartArray(table.TableName);
-                foreach (DataRow row in table.Rows)
-                {
-                    jsonWriter.WriteStartObject();
-                    foreach (DataColumn column in table.Columns)
-                    {
-                        var dataType = column.DataType.Name;
-                        if (row.IsNull(column.ColumnName))
-                        {
-                            dataType = "Null";
-                        }
-                        switch (dataType)
-                        {
-                            case "Decimal":
-                                jsonWriter.WriteNumber(column.ColumnName, (decimal)row[column]);
-                                break;
-                            case "String":
-                                jsonWriter.WriteString(column.ColumnName, row[column].ToString().AsSpan());
-                                break;
-                            case "Boolean":
-                                jsonWriter.WriteBoolean(column.ColumnName, (bool)row[column]);
-                                break;
-                            case "Null":
-                                jsonWriter.WriteNull(column.ColumnName);
-                                break;
-                            default:
-                                throw new NotSupportedException($"Data type {column.DataType.Name} is not supported.");
-                        }
-
-                        //jsonWriter.WriteString(column.ColumnName, row[column].ToString());
-                    }
-                    jsonWriter.WriteEndObject();
-                }
-                jsonWriter.WriteEndArray();
+                WriteTable(jsonWriter, table, true);
             }
             jsonWriter.WriteEndObject();
         }
     }
+
     private static void SaveFolderAsDB(JsonConnection jsonConnection)
     {
         foreach (DataTable table in jsonConnection.JsonReader.DataSet!.Tables)
@@ -85,41 +53,50 @@ public abstract class JsonWriter
             using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
             using (var jsonWriter = new Utf8JsonWriter(fileStream))
             {
-                jsonWriter.WriteStartArray();
-                foreach (DataRow row in table.Rows)
-                {
-                    jsonWriter.WriteStartObject();
-                    foreach (DataColumn column in table.Columns)
-                    {
-                        var dataType = column.DataType.Name;
-                        if (row.IsNull(column.ColumnName))
-                        {
-                            dataType = "Null";
-                        }
-                        switch (dataType)
-                        {
-                            case "Decimal":
-                                jsonWriter.WriteNumber(column.ColumnName, (decimal)row[column]);
-                                break;
-                            case "String":
-                                jsonWriter.WriteString(column.ColumnName, row[column].ToString().AsSpan());
-                                break;
-                            case "Boolean":
-                                jsonWriter.WriteBoolean(column.ColumnName, (bool)row[column]);
-                                break;
-                            case "Null":
-                                jsonWriter.WriteNull(column.ColumnName);
-                                break;
-                            default:
-                                throw new NotSupportedException($"Data type {column.DataType.Name} is not supported.");
-                        }
-
-                        //jsonWriter.WriteString(column.ColumnName, row[column].ToString());
-                    }
-                    jsonWriter.WriteEndObject();
-                }
-                jsonWriter.WriteEndArray();
+                WriteTable(jsonWriter, table, false);
             }
         }
+    }
+
+    private static void WriteTable(Utf8JsonWriter jsonWriter, DataTable table, bool writeTableName)
+    {
+        if (writeTableName)
+            jsonWriter.WriteStartArray(table.TableName);
+        else
+            jsonWriter.WriteStartArray();
+
+        foreach (DataRow row in table.Rows)
+        {
+            jsonWriter.WriteStartObject();
+            foreach (DataColumn column in table.Columns)
+            {
+                var dataType = column.DataType.Name;
+                if (row.IsNull(column.ColumnName))
+                {
+                    dataType = "Null";
+                }
+                switch (dataType)
+                {
+                    case "Decimal":
+                        jsonWriter.WriteNumber(column.ColumnName, (decimal)row[column]);
+                        break;
+                    case "String":
+                        jsonWriter.WriteString(column.ColumnName, row[column].ToString().AsSpan());
+                        break;
+                    case "Boolean":
+                        jsonWriter.WriteBoolean(column.ColumnName, (bool)row[column]);
+                        break;
+                    case "Null":
+                        jsonWriter.WriteNull(column.ColumnName);
+                        break;
+                    default:
+                        throw new NotSupportedException($"Data type {column.DataType.Name} is not supported.");
+                }
+
+                //jsonWriter.WriteString(column.ColumnName, row[column].ToString());
+            }
+            jsonWriter.WriteEndObject();
+        }
+        jsonWriter.WriteEndArray();
     }
 }
