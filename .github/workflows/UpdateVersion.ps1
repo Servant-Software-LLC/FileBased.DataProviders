800a5a8bc7484147ded105dee4c3ee14ddda800a
David Maltby <david.maltby@hotmail.com>
2023-03-18 14:47:30 -0500

    All projects which create NuGet packages need to have build number for their version.

--- .github/workflows/UpdateVersion.ps1
+++ .github/workflows/UpdateVersion.ps1
@@ -9,36 +9,56 @@ if([string]::IsNullOrWhiteSpace($buildNumber)){
     throw "Please specify the version build number to be used"
 }
 
+function Update-CsprojFile {
 
-$dotnetCountersFolder = Join-Path -Path $PSScriptRoot -ChildPath "../../src/Data.Json"
-$csprojFile = Join-Path -Path $dotnetCountersFolder -ChildPath "Data.Json.csproj"
+    param (
+		[Parameter(Position=0, Mandatory)]
+		[ValidateNotNullOrEmpty()]
+        [string]$csprojFile,
+		[Parameter(Position=1, Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$buildNumber
+    )
 
-$csprojXml = [xml](get-content ($csprojFile))
+	$csprojXml = [xml](get-content ($csprojFile))
 
-$versionNode = $csprojXml.Project.PropertyGroup.Version
-if ($versionNode -eq $null) {
-    # create version node if it doesn't exist
-    $versionNode = $csprojXml.CreateElement("Version")
-    $csprojXml.Project.PropertyGroup.AppendChild($versionNode)
-    Write-Host "AssemblyVersion XML tag added to $($csproj)"
+	$versionNode = $csprojXml.Project.PropertyGroup.Version
+	if ($versionNode -eq $null) {
+		# create version node if it doesn't exist
 
-    $version = "1.0.0"
+		$versionNode = $csprojXml.CreateElement("Version")
+		$csprojXml.Project.PropertyGroup.AppendChild($versionNode) > $null
+		#Write-Host "Version XML element added to the PropertyGroup of $($csprojFile)"
+
+		$version = "1.0.0"
+	}
+	else
+	{
+		#Write-Host "Version XML element found in the PropertyGroup of $($csprojFile)"
+		$version = $versionNode
+	}
+
+	$index = $version.LastIndexOf('.')
+	if ($index -eq -1) {
+		throw "$version isn't in the expected build format. $version = $($version))"
+	}
+
+	$version = $version.Substring(0, $index + 1) + $buildNumber
+
+	Write-Host "Stamping $($csproj) with version number $($version)"
+
+	$csprojXml.Project.PropertyGroup.Version = $version
+
+
+	$csprojXml.Save($csprojFile)
 }
-else
-{
-    $version = $csprojXml.Project.PropertyGroup.Version
+
+#Enumerate the csproj files that are under .\src (exclude 3rdParty folder)
+Get-ChildItem "./src" -Exclude "3rdParty" | 
+ForEach-Object {
+    
+  Get-ChildItem $_.FullName -Include *.csproj -Recurse |
+  ForEach-Object {
+    Update-CsprojFile $_.FullName $buildNumber
+  }
 }
-
-$index = $version.LastIndexOf('.')
-if ($index -eq -1) {
-    throw "$version isn't in the expected build format. $version = $($version))"
-}
-
-$version = $version.Substring(0, $index + 1) + $buildNumber
-
-Write-Host "Stamping $($csproj) with version number $($version)"
-
-$csprojXml.Project.PropertyGroup.Version = $version
-
-
-$csprojXml.Save($csprojFile)
