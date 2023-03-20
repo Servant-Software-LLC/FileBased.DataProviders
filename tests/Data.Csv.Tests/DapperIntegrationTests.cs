c206576788c51e9f50f3b4b23c4e52a3076f0158
David Maltby <david.maltby@hotmail.com>
2023-03-19 21:32:46 -0500

    Creating integration tests to replace Console application.

---
+++ tests/Data.Csv.Tests/DapperIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System.Data.CsvClient;
+using Xunit;
+
+namespace Data.Csv.Tests;
+
+public class DapperIntegrationTests
+{
+    [Fact(Skip = "TODO: Since CSV provider does not have FileAsDatabase, we need to have source files for the individual tables that are in the ecommerce database")]
+    public void Query_StronglyTyped()
+    {
+        // Arrange
+        var connection = new CsvConnection(ConnectionStrings.FolderAsDBConnectionString);
+        connection.Open();
+
+        // Act
+        var rows = connection.
+            Query<Records>("SELECT c.CustomerName, [o].[OrderDate], [oi].[Quantity], [p].[Name] FROM [Customers c] INNER JOIN [Orders o] ON [c].[ID] = [o].[CustomerID] INNER JOIN [OrderItems oi] ON [o].[ID] = [oi].[OrderID] INNER JOIN [Products p] ON [p].[ID] = [oi].[ProductID]").
+            ToList();
+
+        foreach (var item in rows)
+        {
+        }
+
+    }
+
+
+    private class Records
+    {
+        public string? CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? Quantity { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ tests/Data.Json.Tests/DapperIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System.Data.JsonClient;
+using Xunit;
+
+namespace Data.Json.Tests;
+
+public class DapperIntegrationTests
+{
+    [Fact]
+    public void Query_StronglyTyped()
+    {
+        // Arrange
+        var connection = new JsonConnection(ConnectionStrings.eComDBConnectionString);
+        connection.Open();
+
+        // Act
+        var rows = connection.
+            Query<Records>("SELECT c.CustomerName, [o].[OrderDate], [oi].[Quantity], [p].[Name] FROM [Customers c] INNER JOIN [Orders o] ON [c].[ID] = [o].[CustomerID] INNER JOIN [OrderItems oi] ON [o].[ID] = [oi].[OrderID] INNER JOIN [Products p] ON [p].[ID] = [oi].[ProductID]").
+            ToList();
+
+        foreach (var item in rows)
+        {
+        }
+    }
+
+
+    private class Records
+    {
+        public string? CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? Quantity { get; set; }
+        public string? Name { get; set; }
+    }
+
+}
