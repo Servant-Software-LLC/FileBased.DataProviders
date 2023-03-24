4de2cf51c5629116d5db0b99415db8391db0d49e
David Maltby <david.maltby@hotmail.com>
2023-03-24 07:24:44 -0500

    Now with an eComm database for CSV, we don't need to skip a Dapper integration test for it.

--- tests/Data.Json.Tests/DapperIntegrationTests.cs
+++ tests/Data.Json.Tests/DapperIntegrationTests.cs
@@ -18,8 +18,15 @@ public class DapperIntegrationTests
             Query<Records>("SELECT c.CustomerName, [o].[OrderDate], [oi].[Quantity], [p].[Name] FROM [Customers c] INNER JOIN [Orders o] ON [c].[ID] = [o].[CustomerID] INNER JOIN [OrderItems oi] ON [o].[ID] = [oi].[OrderID] INNER JOIN [Products p] ON [p].[ID] = [oi].[ProductID]").
             ToList();
 
+        Assert.True(rows.Any());
+
         foreach (var item in rows)
         {
+            //To make sure that the INNER JOIN succeeded, make sure that we have data from each table involved.
+            Assert.False(string.IsNullOrEmpty(item.CustomerName));
+            Assert.False(item.OrderDate == default);
+            Assert.False(item.Quantity == 0);
+            Assert.False(string.IsNullOrEmpty(item.Name));
         }
     }
 
@@ -28,7 +35,7 @@ public class DapperIntegrationTests
     {
         public string? CustomerName { get; set; }
         public DateTime OrderDate { get; set; }
-        public string? Quantity { get; set; }
+        public int Quantity { get; set; }
         public string? Name { get; set; }
     }
 
