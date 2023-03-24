d7757c764bbba32b29e5b7aa12f3a5be23629326
David Maltby <david.maltby@hotmail.com>
2023-03-24 17:46:37 -0500

    Moving Common project to right location.  Upgrade to .NET 7.0

--- EFCore.FileBasedProviders.Common/ConnectionString.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EFCore.FileBasedProviders.Common;
-
-public class ConnectionString
-{
-    public static string Format(string dataSource) => $"DataSource={dataSource};";
-}
---
+++ src/EFCore.FileBasedProviders.Common/ConnectionString.cs
@@ -0,0 +1,6 @@
+﻿namespace EFCore.FileBasedProviders.Common;
+
+public class ConnectionString
+{
+    public static string Format(string dataSource) => $"DataSource={dataSource};";
+}
