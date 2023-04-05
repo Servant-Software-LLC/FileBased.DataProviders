799b81f89b9eb38722879ae7897b72d72c9a411f
David Maltby <david.maltby@hotmail.com>
2023-04-05 15:33:53 -0500

    Folder vs. File as DB connection string mix up.

--- tests/Data.Xml.Tests/FolderAsDatabase/XmlTransactionTests.cs
+++ tests/Data.Xml.Tests/FolderAsDatabase/XmlTransactionTests.cs
@@ -40,6 +40,6 @@ public partial class XmlTransactionTests
     {
         TransactionTests.
                 Transaction_ShouldRollbackWhenExceptionIsThrown(
-            () => new XmlConnection(ConnectionStrings.Instance.FileAsDBConnectionString));
+            () => new XmlConnection(ConnectionStrings.Instance.FolderAsDBConnectionString));
     }
 }
