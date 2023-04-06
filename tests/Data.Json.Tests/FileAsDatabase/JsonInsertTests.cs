b3e8ec32ef18dca4b46a11c498fdb68608e247dd
David Maltby <david.maltby@hotmail.com>
2023-04-06 09:27:52 -0500

    For JsonInsertTests.Insert_ShouldInsertDataIntoEmptyTables() unit test ensure that the schema after a row INSERT fits the data types of the data row provided.

--- tests/Data.Json.Tests/FileAsDatabase/JsonInsertTests.cs
+++ tests/Data.Json.Tests/FileAsDatabase/JsonInsertTests.cs
@@ -1,5 +1,6 @@
 using Data.Common.Extension;
 using Data.Tests.Common;
+using System.Data;
 using System.Data.JsonClient;
 using System.Reflection;
 using Xunit;
@@ -21,7 +22,48 @@ public class JsonInsertTests
     public void Insert_ShouldInsertDataIntoEmptyTables()
     {
         var sandboxId = $"{GetType().FullName}.{MethodBase.GetCurrentMethod()!.Name}";
-        InsertTests.Insert_ShouldInsertData(() => new JsonConnection(ConnectionStrings.Instance.EmptyWithTablesFileAsDB.Sandbox("Sandbox", sandboxId)));
+        var sandboxConnectionString = ConnectionStrings.Instance.EmptyWithTablesFileAsDB.Sandbox("Sandbox", sandboxId);
+        InsertTests.Insert_ShouldInsertData(() => new JsonConnection(sandboxConnectionString));
+
+        //Extra assertions to verify that our JSON tables now have a schema 
+        using (var connection = new JsonConnection(sandboxConnectionString))
+        {
+            //Copy the contents of the tables into a DataSet
+            var adapter = connection.CreateDataAdapter("SELECT * FROM locations");
+            var dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            //Verify that the DataColumns are of the correct schema type.
+            //Table: locations
+            var locationsTable = dataset.Tables[0];
+            var idColumn = locationsTable.Columns["id"];
+            Assert.Equal(typeof(decimal), idColumn.DataType);
+            var cityColumn = locationsTable.Columns["city"];
+            Assert.Equal(typeof(string), cityColumn.DataType);
+            var stateColumn = locationsTable.Columns["state"];
+            Assert.Equal(typeof(string), stateColumn.DataType);
+            var zipColumn = locationsTable.Columns["zip"];
+            Assert.Equal(typeof(decimal), zipColumn.DataType);
+
+            adapter = connection.CreateDataAdapter("SELECT * FROM employees");
+            adapter.Fill(dataset);
+
+            //Table: employees
+            var employeesTable = dataset.Tables[0];
+            var nameColumn = employeesTable.Columns["name"];
+            Assert.Equal(typeof(string), nameColumn.DataType);
+            var emailColumn = employeesTable.Columns["email"];
+            Assert.Equal(typeof(string), emailColumn.DataType);
+            var salaryColumn = employeesTable.Columns["salary"];
+            Assert.Equal(typeof(decimal), salaryColumn.DataType);
+            var marriedColumn = employeesTable.Columns["married"];
+
+            //NOTE:  When https://github.com/Servant-Software-LLC/ADO.NET.FileBased.DataProviders/issues/22 is solved, then 
+            //       this assert will fail, because it needs to have an expectation of:
+            // Assert.Equal(typeof(boolean), marriedColumn.DataType);
+            Assert.Equal(typeof(string), marriedColumn.DataType);
+        }
+
     }
 
     [Fact]
