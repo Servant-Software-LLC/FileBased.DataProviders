c1d63d227a4feca6740500334af2437c5d1b03f7
David Maltby <david.maltby@hotmail.com>
2023-05-20 17:05:04 -0500

    Copying sample from ChatGPT

---
+++ src/EFCore.Csv/Scaffolding/Internal/CsvDatabaseModelFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Csv.Scaffolding.Internal;
+
+public class CsvDatabaseModelFactory : IDatabaseModelFactory
+{
+    public DatabaseModel Create(string connectionString, DatabaseModelFactoryOptions options)
+    {
+        // Implementation goes here
+    }
+
+    public DatabaseModel Create(DbConnection connection, DatabaseModelFactoryOptions options)
+    {
+        // Implementation goes here
+    }
+}
