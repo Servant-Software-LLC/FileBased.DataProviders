ccbe3866d5dfc4de2868619df3c6dbe5ca67554f
David Maltby <david.maltby@hotmail.com>
2024-11-13 15:54:05 -0600

    Remove duplicate code.

--- src/Data.Xml/XmlIO/Write/XmlInsert.cs
+++ src/Data.Xml/XmlIO/Write/XmlInsert.cs
@@ -14,19 +14,8 @@ internal class XmlInsert : Common.FileIO.Write.FileInsertWriter
 
     protected override object DefaultIdentityValue() => 1;
 
-    protected override bool DecimalHandled(DataColumn dataColumn, DataRow lastRow, DataRow newRow)
-    {
-        if (dataColumn.DataType == typeof(decimal))
-        {
-            var lastRowColumnValue = lastRow[dataColumn.ColumnName];
-            if (lastRowColumnValue is decimal decValue)
-                newRow[dataColumn.ColumnName] = decValue + 1m;
-
-            return true;
-        }
-
-        return false;
-    }
+    protected override bool DecimalHandled(DataColumn dataColumn, DataRow lastRow, DataRow newRow) =>
+        Default_DecimalHandled(dataColumn, lastRow, newRow);
 
     protected override void RealizeSchema(DataTable dataTable) => throw new NotImplementedException();
 
